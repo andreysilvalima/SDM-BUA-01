ca6b2e0cb3417bd28e5b87573734e93fd0f23aea
andreysilvalima <andreyslima1609@gmail.com>
2025-03-11 10:13:22 -0300

    Feat: Trabalhando com variaveis

---
+++ variaveis.js
@@ -0,0 +1,18 @@
+var idade = 19;
+let nome = "Andrey";
+const PI = 3.14; 
+let ativo = true
+console.log(idade, nome, PI, ativo);
+if (true){
+    let cidade = "cotia";
+    console.log(cidade);
+}
+//Convertendo numeros
+let int = parseInt("10.5")
+let float = parseFloat("7.5")
+let num = Number("42")
+//Covertendo numero para string
+
+let str = String(100)
+let outrstr = (42).toString()
+
